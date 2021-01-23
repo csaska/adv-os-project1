c530e7af2483309af07a2904e832f4bd4a81646e
Colton Saska <colt.saska@gmail.com>
2021-01-23 13:58:33 -0500

    Initial wcat commit

--- utilities/wcat/wcat.c
+++ utilities/wcat/wcat.c
@@ -1,1 +1,25 @@
-//Hey your code goes here..!
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_file(FILE *in_fp, FILE *out_fp) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread = 0;
+    while ((nread = getline(&line, &len, in_fp)) != -1) {
+        fprintf(out_fp, "%s", line);
+    }
+    free(line);
+}
+
+int main(int argc, char *argv[]) {
+    for (int idx = 1; idx < argc; ++idx) {
+        char *filename = argv[idx];
+
+        FILE *fp = fopen(filename, "r");
+        if (fp == NULL) {
+            fprintf(stdout, "wcat: cannot open file\n");
+            return 1;
+        }
+        print_file(fp, stdout);
+    }
+}
